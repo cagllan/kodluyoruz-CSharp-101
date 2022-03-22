56113270922026b7915face63c3487d9f9b68497
Caglan Atay <caglanatay@windowslive.com>
2022-03-23 00:44:15 +0300

    csharp-enum

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gunler.Pazar);
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+            if(sicaklik <= (int)HavaDurumu.Normal)
+                System.Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if (sicaklik >= (int)HavaDurumu.Sıcak)
+                System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak)
+                System.Console.WriteLine("Hadi dışarıya çıkalım");            
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSicak = 30
+    }
+}
+
