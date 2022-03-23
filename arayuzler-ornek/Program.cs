54175358efb7a6e09b8060cfb196546cf504909e
Caglan Atay <caglanatay@windowslive.com>
2022-03-23 22:13:45 +0300

    arayuzler-ornek

---
+++ arayuzler-ornek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace arayuzler_ornek
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Focus focus = new Focus();
+            System.Console.WriteLine(focus.HangiMarkaninAraci().ToString());
+            System.Console.WriteLine(focus.KacTekerlektenOlusur());
+            System.Console.WriteLine(focus.StandartRengiNe().ToString());
+
+
+            Civic civic = new Civic();
+            System.Console.WriteLine(civic.HangiMarkaninAraci().ToString());
+            System.Console.WriteLine(civic.KacTekerlektenOlusur());
+            System.Console.WriteLine(civic.StandartRengiNe().ToString());
+        }
+    }
+}
---
+++ arayuzler-ornek/Sabitler.cs
@@ -0,0 +1,17 @@
+// bu class ın içerisine enum ları yazalım
+
+namespace arayuzler_ornek
+{
+    public enum Marka
+    {
+        Ford,
+        Toyota,
+        Honda
+    }
+
+    public enum Renk
+    {
+        Beyaz,
+        Gri
+    }
+}
